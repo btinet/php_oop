549683bb94ee31814ec2bf166a4d9f6f466f2c47
Benjamin Wagner <51082373+btinet@users.noreply.github.com>
2023-05-18 18:21:34 +0200

    update

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -3,29 +3,43 @@
 namespace App;
 
 use App\Model\Request;
+use LogicException;
 
 class Bootstrap
 {
 
     protected Request $request = Request::GET;
 
-    protected  $controller;
+    protected $controller;
 
     public function __construct()
     {
         
-        if(!$this->request->get("controller")) die ("Controller fehlt!");
+        try {
+            $this->getClass()->runMethod();
+        } catch (LogicException $e)
+        {
+            echo $e->getMessage();
+        }
 
+    }
+
+    protected function getClass(): Bootstrap
+    {
+        if(!$this->request->get("controller")) throw new LogicException("Key \"controller\" nicht gesetzt!");
         $controllerClass = "App\\Controller\\" . ucfirst($this->request->get("controller")) . "Controller";
-        if(!class_exists($controllerClass)) echo "Klasse nicht verfügbar!";        
-
+        if(!class_exists($controllerClass)) throw new LogicException("Controller {$controllerClass} existiert nicht!"); 
         $this->controller = new $controllerClass();
 
-        if(!$this->request->get("action")) $method = 'index';
-        if(!method_exists($this->controller,$method)) die ("Methode nicht verfügbar!");
+        return $this;
+    }
+
+    protected function runMethod(): void
+    {
+        if(!$method = $this->request->get("action")) $method = 'index';
+        if(!method_exists($this->controller,$method)) throw new LogicException("Methode {$method} existiert nicht!");
 
         $this->controller->$method();
-
     }
 
 }
