291ccb69c2a6fcae96c78d5c7012d65c5b52f14d
Informatik User 1 <info-git1-tk@treptowkolleg.de>
2023-05-24 05:43:46 +0200

    add Computer entity

---
+++ src/Entity/Computer.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+class Computer
+{
+
+    # Attribute
+
+    private string $producerName;
+    private int $ramSize;
+    private int $hardDiskSize;
+    private int $usbPorts;
+
+
+    # Getter und Setter
+
+    public function getProducerName(): string
+    {
+        return $this->producerName;
+    }
+
+    public function setProducerName(string $producerName): void
+    {
+        $this->producerName = $producerName;
+    }
+
+    public function getRamSize(): int
+    {
+        return $this->ramSize;
+    }
+
+    public function setRamSize(int $ramSize): void
+    {
+        $this->ramSize = $ramSize;
+    }
+
+    public function getHardDiskSize(): int
+    {
+        return $this->hardDiskSize;
+    }
+
+    public function setHardDiskSize(int $hardDiskSize): void
+    {
+        $this->hardDiskSize = $hardDiskSize;
+    }
+
+    public function getUsbPorts(): int
+    {
+        return $this->usbPorts;
+    }
+
+    public function setUsbPorts(int $usbPorts): void
+    {
+        $this->usbPorts = $usbPorts;
+    }
+
+}
